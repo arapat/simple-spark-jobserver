eb0fb6e7ca71072d6c9855c6a776033cd979b357
Julaiti Alafate <jalafate@gmail.com>
2016-03-29 09:08:30 -0700

    added last_completed logger to avoid redundant result write

--- server/executor.py
+++ server/executor.py
@@ -11,6 +11,8 @@ from Constants import APP_STATUS_API, SUBMIT_COMMAND
 db = None
 to_file_id = None
 
+last_completed = None
+
 
 def load_db():
     global db, to_file_id
@@ -32,7 +34,7 @@ def get_all_files(dir_name):
 
 
 def update_result(result, app_name):
-    spark_id = result['id']
+    spark_id = result["id"]
     if (app_name, spark_id) not in db["runs"]:
         to_file_id[spark_id] = app_name
         db["runs"].append((app_name, spark_id))
@@ -41,13 +43,14 @@ def update_result(result, app_name):
         "appId": app_name,
         "runId": spark_id,
         "name": result["name"],
-        "startTime": result['attempts'][0]['startTime'],
-        "endTime": result['attempts'][0]['endTime']
+        "startTime": result["attempts"][0]["startTime"],
+        "endTime": result["attempts"][0]["endTime"]
     }
     if result["attempts"][0]["completed"]:
-        r['stdout'] = open(
+        r["status"] = "completed"
+        r["stdout"] = open(
             os.path.join(OUTPUT_FOLDER, app_name + "-stdout.txt")).read()
-        r['stderr'] = open(
+        r["stderr"] = open(
             os.path.join(OUTPUT_FOLDER, app_name + "-stderr.txt")).read()
     else:
         r["status"] = "running"
@@ -78,10 +81,13 @@ def run_program(queue_length):
 
 
 def refresh():
+    global last_completed
     status = get_app_status()
     if status[0]["attempts"][0]["completed"]:
         spark_id = status[0]["id"]
-        update_result(status[0], to_file_id[spark_id])
+        if last_completed != spark_id:
+            last_completed = spark_id
+            update_result(status[0], to_file_id[spark_id])
         run_program(len(status))
 
 
