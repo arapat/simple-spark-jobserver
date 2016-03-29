af09c36883f0ed4e6d05ef1f8c851d10af62e8ac
Julaiti Alafate <jalafate@gmail.com>
2016-03-29 09:53:26 -0700

    minor refactor

--- server/Constants.py
+++ server/Constants.py
@@ -12,11 +12,12 @@ RESULT_FOLDER = "/path/to/the/results"
 ALLOWED_EXTENSIONS = set(["py"])
 
 SPARK_PATH = "/path/to/spark"
-MASTER_IP = "spark://<master_ip>:7077"
-SUBMIT_COMMAND = (SPARK_PATH + "/bin/spark-submit --master " + MASTER_IP +
+MASTER_IP = "<master_ip>"
+MASTER_URL = "spark://%s:7077" % MASTER_IP
+SUBMIT_COMMAND = (SPARK_PATH + "/bin/spark-submit --master " + MASTER_URL +
                   " %s 1> %s 2> %s & disown")
 
-SPARK_SERVER = "http://<master_ip>:8080/api/v1"
+SPARK_SERVER = "http://%s:8080/api/v1" % MASTER_IP
 APP_STATUS_API = SPARK_SERVER + "/applications"
 
 INIT_DB = {"runs": []}
